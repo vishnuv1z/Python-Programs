3bbbb87a4dcf4ef7532e8a53434037922b823bac
Vishnu Anup <vishnuanup06@gmail.com>
2025-09-15 15:57:43 +0530

    Update append.py

--- append.py
+++ append.py
@@ -1,9 +1,9 @@
-'''a = [1, 2, 3, 4, 5]
+a = [1, 2, 3, 4, 5]
 a.append(6)
 print(a)
 b = int(input("Enter the element you want to remove: "))
 a.remove(b)
-print(a)'''
+print(a)
 
 a = [2, 1, 8]
 b = max([2, 1, 8])
